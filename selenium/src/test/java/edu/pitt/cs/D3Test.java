677fa1b1ee2f59859cf4481dcc324f38922a3620
luke <luke8499@gmail.com>
2024-04-04 13:53:24 -0400

    imported ChromeOptions

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -24,6 +24,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeOptions; 
 import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
